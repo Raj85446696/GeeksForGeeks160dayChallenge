7e2256bbcde2c195c7725d5d3016d7d881f69c18
RITU RAJ KUMAR <146192378+Raj85446696@users.noreply.github.com>
2025-06-26 12:02:28 +0530

    Add files via upload

--- Tree.java
+++ Tree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Tree {
     class TreeNode {
         int data;
@@ -21,8 +23,27 @@ class Tree {
     }
 
     public int height(TreeNode root) {
-        if (root == null || (root.leftChild == null && root.rightChild == null)) return 0;
+        if (root == null || (root.leftChild == null && root.rightChild == null))
+            return 0;
         return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    // Level order Traversal
+    public void LevelOrder(TreeNode root) {
+        if (root == null)
+            return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            System.out.print(curr.data + " ");
+            if (curr.leftChild != null) {
+                queue.add(curr.leftChild);
+            }
+            if (curr.rightChild != null) {
+                queue.add(curr.rightChild);
+            }
+        }
     }
 
     // Diameter of Binary Tree
@@ -33,27 +54,109 @@ class Tree {
         int rightHeight = height(root.rightChild);
         int leftDia = diamterTree(root.leftChild);
         int rightDia = diamterTree(root.rightChild);
-        int currDia = leftHeight + rightHeight ; 
-        return Math.max(currDia,Math.max(leftDia,rightDia));
+        int currDia = leftHeight + rightHeight;
+        return Math.max(currDia, Math.max(leftDia, rightDia));
     }
 
-    // Tree is balanced 
-    public boolean isBalanced(TreeNode root){
-        if(root==null) return true ; 
+    // Tree is balanced
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
         int leftHeight = height(root.leftChild);
         int rightHeight = height(root.rightChild);
-        int diff = Math.abs(leftHeight - rightHeight); 
-        if(diff>1)return false ; 
+        int diff = Math.abs(leftHeight - rightHeight);
+        if (diff > 1)
+            return false;
         return isBalanced(root.leftChild) && isBalanced(root.rightChild);
     }
 
-    // Tree is Same 
-    public boolean isSame(TreeNode root , TreeNode root2){
-        if(root==null && root2==null) return true ; 
-        if(root==null || root2==null) return false ; 
-        if(root.data!=root2.data) return false ; 
+    // Tree is Same
+    public boolean isSame(TreeNode root, TreeNode root2) {
+        if (root == null && root2 == null)
+            return true;
+        if (root == null || root2 == null)
+            return false;
+        if (root.data != root2.data)
+            return false;
         return isSame(root.leftChild, root2.leftChild) && isSame(root.rightChild, root2.rightChild);
     }
+
+    // Binary Tree Path
+    public List<String> binaryTreePath(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        helper(root, ans, "");
+        return ans;
+    }
+
+    public void helper(TreeNode root, List<String> ans, String s) {
+        if (root == null)
+            return;
+        if (root.leftChild == null && root.rightChild == null) {
+            ans.add(s);
+            return;
+        }
+        helper(root.leftChild, ans, s + root.data + "->");
+        helper(root.rightChild, ans, s + root.data + "->");
+    }
+
+    // Lowest Common Ancestor
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == root || q == root)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.leftChild, p, q);
+        TreeNode right = lowestCommonAncestor(root.rightChild, p, q);
+        if (left != null && right != null)
+            return root;
+        return (left != null) ? left : right;
+    }
+
+    // Invert Binary Tree
+    public TreeNode invertBinaryTree(TreeNode root) {
+        if (root == null)
+            return root;
+        TreeNode left = root.leftChild;
+        TreeNode right = root.rightChild;
+        root.leftChild = invertBinaryTree(left);
+        root.rightChild = invertBinaryTree(right);
+        return root;
+    }
+
+    // Symmetric Tree
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        root.leftChild = invertBinaryTree(root.leftChild);
+        return isSame(root.leftChild, root.rightChild);
+    }
+
+    public void printLevel(TreeNode node, int level, boolean leftToRight) {
+        if (node == null)
+            return;
+
+        if (level == 1) {
+            System.out.print(node.data + " ");
+        } else {
+            if (leftToRight) {
+                printLevel(node.leftChild, level - 1, leftToRight);
+                printLevel(node.rightChild, level - 1, leftToRight);
+            } else {
+                printLevel(node.rightChild, level - 1, leftToRight);
+                printLevel(node.leftChild, level - 1, leftToRight);
+            }
+        }
+    }
+
+    // Zig Zak Order
+    public void ZigZakOrder(TreeNode root) {
+        int level = height(root);
+        boolean leftToRight = true;
+        for (int i = 1; i <= level; i++) {
+            printLevel(root, level, leftToRight);
+            leftToRight=!leftToRight;
+
+        }
+    }
+
     public static void main(String[] args) {
         Tree t = new Tree();
         TreeNode root = t.new TreeNode(1);
@@ -62,6 +165,7 @@ class Tree {
         TreeNode c = t.new TreeNode(4);
         TreeNode d = t.new TreeNode(5);
         TreeNode e = t.new TreeNode(6);
+        TreeNode f = t.new TreeNode(7);
 
         TreeNode root2 = t.new TreeNode(1);
         TreeNode a1 = t.new TreeNode(2);
@@ -74,7 +178,8 @@ class Tree {
         root.rightChild = b;
         a.leftChild = c;
         a.rightChild = d;
-        d.rightChild = e;
+        b.rightChild = e;
+        b.leftChild = f;
 
         root2.leftChild = a1;
         root2.rightChild = b1;
@@ -84,10 +189,17 @@ class Tree {
 
         t.Inorder(root);
         System.out.println(" ");
-        System.out.println("Height of TreeNode is :- "+t.height(root));
-        System.out.println("Diamter of TreeNode :- "+t.diamterTree(root));
+        System.out.println("Height of TreeNode is :- " + t.height(root));
+        System.out.println("Diamter of TreeNode :- " + t.diamterTree(root));
         System.out.println(t.isBalanced(root));
-        System.out.println("Tree is Same or Not :- "+t.isSame(root,root2));
+        System.out.println("Tree is Same or Not :- " + t.isSame(root, root2));
+        System.out.println(t.binaryTreePath(root));
+        System.out.println(t.lowestCommonAncestor(root, c, b).data);
+        t.invertBinaryTree(root);
+        // t.Inorder(root);
+        System.out.println(t.isSymmetric(root));
+        t.LevelOrder(root);
+        t.ZigZakOrder(root);
 
     }
 }
